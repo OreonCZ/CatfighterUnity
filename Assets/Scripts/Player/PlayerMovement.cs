60980552b1bd5d421e87dc1c2d3e2625301689d9
OreonCZ <filipkucera5@gmail.com>
2023-10-19 22:39:26 +0200

    Added sprint bar
    Still in progress

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
@@ -8,23 +9,57 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     bool isMoving = false;
     public Animator animator;
+    public Slider slider;
+    public int maxStamina = 100;
+    public int currentStamina;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentStamina = maxStamina;
+        slider.maxValue = maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value = currentStamina;
+
+        //movement
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(moveX, moveY).normalized;
+       
+       
+         
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+            rb.velocity = moveInput * (movementSpeed * 2) * Time.fixedDeltaTime;
+            }
+             else
+            {
+            rb.velocity = moveInput * movementSpeed * Time.fixedDeltaTime;
+            }
 
+/*
+if(currentStamina > 0) { 
+switch (Input.GetKey(KeyCode.LeftShift))
+{
+    case true:
+        rb.velocity = moveInput * (movementSpeed * 2) * Time.fixedDeltaTime;
+        currentStamina-=1;
+        break;
+    case false:
+        rb.velocity = moveInput * movementSpeed * Time.fixedDeltaTime;
+        currentStamina+=1;
+        break;
+}
+}
+*/
 
-        void stopMoving()
+void stopMoving()
         {
             animator.SetBool("WalkingRight", false);
             animator.SetBool("Walking", false);
@@ -32,6 +67,7 @@ public class Movement : MonoBehaviour
             animator.SetBool("WalkingUp", false);
         }
 
+        //animation changer
         if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("WalkingUp", true);
@@ -67,9 +103,9 @@ public class Movement : MonoBehaviour
             stopMoving();
         }
 
-        Vector2 moveInput = new Vector2(moveX, moveY).normalized;
-
-        rb.velocity = moveInput * movementSpeed * Time.fixedDeltaTime;
-
-    }
+        }
 }
+
+
+
+
